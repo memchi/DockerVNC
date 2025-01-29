48dddbc002b7137ae6a6a99a46d6dd8e659ba17c
Martin Emch <memch@gmx.ch>
2025-01-29 20:40:10 +0100

    run and stop

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # DockerVNC
 
-How to make a Docker container with VNC access
+## How to make a Docker container with VNC access
 
 see: <https://medium.com/@gustav0.lewin/how-to-make-a-docker-container-with-vnc-access-f607958141ae>
+
+## Start the VNC server
+
+To start the VNC server, it’s necessary to execute the script start-vnc.sh.
+You can automate this process by specifying an entrypoint in the Dockerfile.
+However, if you prefer manual execution like me:
+docker exec -ti vnc_ubuntu /bin/bash
+
+Once you have access to the container’s CLI:
+`./start-vnc.sh`
+
+Now you can use a VNC client in the host machine to access your container
+via localhost:5901 (tested with RealVNC Viewer).
+The password was defined in the Dockerfile as “password”.
--- Run.bat
+++ Run.bat
@@ -3,26 +3,41 @@ echo ---------------------------------------------------------------------------
 echo %DATE% %TIME% %~nx0 for user: "%USERNAME%" on computer: "%COMPUTERNAME%" starting
 echo --------------------------------------------------------------------------------
 
-:: Now you can build an image called vnc_ubuntu:
+echo "Now you can build an image called vnc_ubuntu"
 docker build -t vnc_ubuntu .
 
-:: Run the container with this command:
+IF errorlevel 1 (
+    echo "docker build not passing"
+    goto PAUSEEND
+)
+
+echo "Run the container with this command"
 docker run -dt --rm --name vnc_ubuntu -p 5901:5901 vnc_ubuntu
 
-:: To start the VNC server, it’s necessary to execute the script start-vnc.sh.
-:: You can automate this process by specifying an entrypoint in the Dockerfile. 
-:: However, if you prefer manual execution like me:
-:: docker exec -ti vnc_ubuntu /bin/bash
+IF errorlevel 1 (
+    echo "docker run not passing"
+    goto PAUSEEND
+)
 
-:: Once you have access to the container’s CLI:
-:: # ./start-vnc.sh
+echo "List the running containers"
+docker ps
 
-:: Now you can use a VNC client in the host machine to access your container 
-:: via localhost:5901 (tested with RealVNC Viewer). 
-:: The password was defined in the Dockerfile as “password”.
-
-:: start vncviewer and point to localhost:5901
+echo "start vncviewer and point to localhost:5901"
 vncviewer.exe -connect localhost:5901 -password password
+
+echo "Stop a running Docker container"
+docker stop vnc_ubuntu
+
+echo "List the running containers"
+docker ps
+
+::echo "Remove the container"
+::docker rm vnc_ubuntu  
+
+goto END
+
+:PAUSEEND
+pause
 
 :END
 echo --------------------------------------------------------------------------------
